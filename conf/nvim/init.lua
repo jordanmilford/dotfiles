d645caffb4dcddea28cddaa49e22bfe42c1932a3
Jordan Davis <jordanmilforddavis@gmail.com>
2023-02-27 19:04:59 -0800

    update nvim-tree settings

--- conf/nvim/init.lua
+++ conf/nvim/init.lua
@@ -218,6 +218,24 @@ vim.g.nvim_tree_icons = {
 -- nvim-tree keymaps
 vim.api.nvim_set_keymap('n', '-', ':NvimTreeFindFile<CR>', keymapSilentNore)
 
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 -- Configure barbar.nvim
 vim.g.bufferline = {
   closable = false
