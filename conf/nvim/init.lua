602de1c57450f931084d01ca45f1da779fb1e0c9
Jordan Davis <jordanmilforddavis@gmail.com>
2024-01-18 21:28:53 -0800

    remove luasnip from nvim config

--- conf/nvim/init.lua
+++ conf/nvim/init.lua
@@ -146,10 +146,14 @@ require("lazy").setup({
   'nvim-treesitter/nvim-treesitter-textobjects', -- Additional textobjects for treesitter
   'neovim/nvim-lspconfig', -- Collection of configurations for built-in LSP client
   'onsails/lspkind.nvim', -- nerd icons in cmp
-  'hrsh7th/nvim-cmp', -- Autocompletion plugin
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    -- Rest of your plugin spec
+  },
   'hrsh7th/cmp-nvim-lsp',
-  'saadparwaiz1/cmp_luasnip',
-  'L3MON4D3/LuaSnip', -- Snippets plugin
+  -- 'saadparwaiz1/cmp_luasnip',
+  -- 'L3MON4D3/LuaSnip', -- Snippets plugin
   'rafamadriz/friendly-snippets', -- vscode format snippets
   {
     "zbirenbaum/copilot.lua",
@@ -603,9 +607,13 @@ lspconfig.lua_ls.setup {
 }
 
 -- luasnip setup
-local luasnip = require 'luasnip'
-require("luasnip.loaders.from_vscode").lazy_load()
-
+-- local luasnip = require 'luasnip'
+-- require("luasnip.loaders.from_vscode").lazy_load()
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
@@ -615,31 +623,26 @@ local lspkind = require('lspkind')
 cmp.setup {
   snippet = {
     expand = function(args)
-      luasnip.lsp_expand(args.body)
+      -- luasnip.lsp_expand(args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
       else
         fallback()
       end
-    end,
+    end),
     ['<S-Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+      -- elseif luasnip.jumpable(-1) then
+        -- luasnip.jump(-1)
       else
         fallback()
       end
@@ -661,7 +664,7 @@ cmp.setup {
   sources = {
     { name = "copilot", group_index = 2 },
     { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    -- { name = 'luasnip' },
   }
 }
 
