93b333b51e681a8f9bb882101ff7b1ce659a8bb5
Jordan Davis <jordanmilforddavis@gmail.com>
2024-03-27 22:52:16 -0700

    restore cmp autopairs function

--- conf/nvim/lua/cmp.lua
+++ conf/nvim/lua/cmp.lua
@@ -3,6 +3,12 @@ local cmp = require 'cmp'
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local lspkind = require('lspkind')
 
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+
 cmp.setup {
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
