e60aed4d5d3bea2c590cbcde211c6fabc249313e
Jordan Davis <jordanmilforddavis@gmail.com>
2024-05-08 20:41:03 -0700

    reenable nvim copilot

--- conf/nvim/lua/plugins/nvim-cmp.lua
+++ conf/nvim/lua/plugins/nvim-cmp.lua
@@ -51,7 +51,7 @@ return {
           })
         },
         sources = {
-          -- { name = "copilot", group_index = 1 },
+          { name = "copilot", group_index = 1 },
           { name = 'nvim_lsp', group_index = 2 },
           -- { name = 'luasnip' },
         }
