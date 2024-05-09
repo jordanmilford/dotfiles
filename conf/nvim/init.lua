accc241cf32d51b4469aa78fe0ca79895f19dc5a
Jordan Davis <jordanmilforddavis@gmail.com>
2024-05-08 20:38:57 -0700

    load vim config core before lazy

--- conf/nvim/init.lua
+++ conf/nvim/init.lua
@@ -11,8 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require('vim-core')
 require("lazy").setup('plugins')
-require('core')
 require('colorschemes')
 require('keymaps')
 require('lsp')
